f80ac1f2e6a8b09fd1d7d66a66ef4be5719700cc
Sakada Ly <lysakada@gmail.com>
2019-09-20 17:31:45 +0200

    version 0.8

--- casse_brique.ino
+++ casse_brique.ino
@@ -14,7 +14,7 @@
 
 
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_RGBW + NEO_KHZ800);
-// Argument 1 = Number of pixels in NeoPixel strip
+// Argument 1 = Number of pixels in NeoPixel pixels
 // Argument 2 = Arduino pin number (most are valid)
 // Argument 3 = Pixel type flags, add together as needed:
 //   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
@@ -25,32 +25,32 @@ Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_RGBW + NEO_KHZ800);
 
 //tableau des calculs
 uint8_t tableau[5][5] = {
-  {1, 0, 0, 0, 1},
-  {1, 2, 2, 2, 1},
-  {1, 0, 0, 0, 1},
-  {1, 0, 3, 0, 1},
-  {1, 0, 0, 0, 1}
+  {0, 0, 2, 0, 0},
+  {0, 2, 0, 2, 0},
+  {0, 0, 2, 0, 0},
+  {0, 0, 0, 3, 0},
+  {0, 0, 4, 0, 0}
 };
 
 //tableau final
 uint8_t tableau_1d[25];
 
-uint8_t briques[3][2] = {
-  {1,1}, {1,2}, {1,3}
+uint8_t briques[4][2] = {
+  {0,2}, {1,1}, {1,3},{2,2}
 };
 
 int led = 13;
 
 //Nous initialisons le positionnemnet du joueur ici
-int joueurX = 1;
-int joueurY = 1;
+int joueurX = 2;
+int joueurY = 4;
 
 //Nous initialisons la position de la balle ici
 uint8_t balleX = 3;
-uint8_t balleY = 2;
+uint8_t balleY = 3;
 
 //Les vitesses initiales de la balle
-int vitesseX = 1;
+int vitesseX = -1;
 int vitesseY = 1;
 
 //initialisation des boutons de déplacement
@@ -80,7 +80,7 @@ void setup() {
   pixels.begin();
   pixels.show(); // Initialize all pixels to 'off'
   pixels.setBrightness(BRIGHTNESS); // Set BRIGHTNESS to about 1/5 (max = 255)
-
+  feuDartifice();
 }
 
 void loop() {
@@ -105,7 +105,14 @@ void loop() {
   }
 
 
-// Tranformation du tableau 
+
+ 
+  
+  size_t array_length = sizeof(tableau_1d) / sizeof(tableau_1d[0]);
+  
+  deplacerBalle();
+
+  // Tranformation du tableau 
   int i, j, k = 0;
 
   for(j = 0 ; j < 5 ; j++){
@@ -121,20 +128,74 @@ void loop() {
       }
     }
   }
- 
-  
-  size_t array_length = sizeof(tableau_1d) / sizeof(tableau_1d[0]);
-  
-  deplacerBalle();
   
   setMatriceCouleur(tableau_1d, array_length);
   
   montrerSerial(); // Permet de vérifier les positions
   
-  delay(2000);
+  delay(500);
 
 }
 
+
+void whiteOverRainbow(int whiteSpeed, int whiteLength) {
+
+  if(whiteLength >= pixels.numPixels()) whiteLength = pixels.numPixels() - 1;
+
+  int      head          = whiteLength - 1;
+  int      tail          = 0;
+  int      loops         = 3;
+  int      loopNum       = 0;
+  uint32_t lastTime      = millis();
+  uint32_t firstPixelHue = 0;
+
+  for(;;) { // Repeat forever (or until a 'break' or 'return')
+    for(int i=0; i<pixels.numPixels(); i++) {  // For each pixel in pixels...
+      if(((i >= tail) && (i <= head)) ||      //  If between head & tail...
+         ((tail > head) && ((i >= tail) || (i <= head)))) {
+        pixels.setPixelColor(i, pixels.Color(0, 0, 0, 255)); // Set white
+      } else {                                             // else set rainbow
+        int pixelHue = firstPixelHue + (i * 65536L / pixels.numPixels());
+        pixels.setPixelColor(i, pixels.gamma32(pixels.ColorHSV(pixelHue)));
+      }
+    }
+
+    pixels.show(); // Update pixels with new contents
+    // There's no delay here, it just runs full-tilt until the timer and
+    // counter combination below runs out.
+
+    firstPixelHue += 40; // Advance just a little along the color wheel
+
+    if((millis() - lastTime) > whiteSpeed) { // Time to update head/tail?
+      if(++head >= pixels.numPixels()) {      // Advance head, wrap around
+        head = 0;
+        if(++loopNum >= loops) return;
+      }
+      if(++tail >= pixels.numPixels()) {      // Advance tail, wrap around
+        tail = 0;
+      }
+      lastTime = millis();                   // Save time of last movement
+    }
+  }
+}
+
+void colorWipe(uint32_t color, int wait) {
+  for(int i=0; i<pixels.numPixels(); i++) { // For each pixel in pixels...
+    pixels.setPixelColor(i, color);         //  Set pixel's color (in RAM)
+    pixels.show();                          //  Update pixels to match
+    delay(wait);                           //  Pause for a moment
+  }
+}
+
+void feuDartifice(){
+  colorWipe(pixels.Color(255,   0,   0)     , 50); // Red
+  colorWipe(pixels.Color(  0, 255,   0)     , 50); // Green
+  colorWipe(pixels.Color(  0,   0, 255)     , 50); // Blue
+  colorWipe(pixels.Color(  0,   0,   0, 255), 50); // True white (not RGB white)
+
+  whiteOverRainbow(75, 5);
+}
+  
 
 void setMatriceCouleur(uint8_t matrice[], int longueurTableau) {
 
@@ -142,16 +203,16 @@ void setMatriceCouleur(uint8_t matrice[], int longueurTableau) {
     if (matrice[i] == 1 ) { 
       pixels.setPixelColor(i, pixels.Color(0, 255, 0)); // Les murs rouges
     } else if (matrice[i] == 2) { 
-      pixels.setPixelColor(i, pixels.Color(255, 255, 0)); // Le joueur 
+      pixels.setPixelColor(i, pixels.Color(255, 255, 0)); // La briques
     } else if (matrice[i] == 3) { 
-      pixels.setPixelColor(i, pixels.Color(255, 0, 0)); //La balle
+      pixels.setPixelColor(i, pixels.Color(255, 0, 255)); //La balle
     } else if (matrice[i] == 4) {
-      pixels.setPixelColor(i, pixels.Color(0, 0, 255)); // Les briques
+      pixels.setPixelColor(i, pixels.Color(0, 0, 255)); // Le joueur
     } else {
       pixels.setPixelColor(i, pixels.Color(0, 0, 0)); // Rien
     }
   }
-  pixels.show();                          //  Update strip to match
+  pixels.show();                          //  Update pixels to match
 }
 
 void deplacerBalle() {
@@ -179,7 +240,7 @@ void deplacerBalle() {
 
 //Si la balle rencontre une brique
 int b,c = 0 ;
-for (b = 0 ; b < 3 ; b ++){
+for (b = 0 ; b < 4 ; b ++){
   if (balleX == briques[b][0] && balleY == briques[b][1]){
       tableau[briques[b][0]][briques[b][1]] = 0 ;
       briques[b][0] = -1;
@@ -199,19 +260,25 @@ if (balleX < 0) {
  // restart()
 }
 
-void restart() {
-  tableau[5][5] = {
+  // Place la lumière sur la nouvelle position de la balle 
+  tableau[balleX][balleY] = 3;
+  
+}
+
+
+/*void restart() {
+  tableau= {
     {1, 0, 0, 0, 1},
     {1, 2, 2, 2, 1},
     {1, 0, 0, 0, 1},
     {1, 0, 3, 0, 1},
     {1, 0, 0, 0, 1}
-  };
+  }
 
   //tableau final
   tableau_1d[25];
 
-  briques[3][2] = {
+  briques= {
     {1,1}, {1,2}, {1,3}
   };
 
@@ -224,11 +291,7 @@ void restart() {
   balleX = 3;
   balleY = 2;
 
-}
-  // Place la lumière sur la nouvelle position de la balle 
-  tableau[balleX][balleY] = 3;
-  
-}
+}*/
 
 void montrerSerial() {
   Serial.println("");
@@ -254,12 +317,12 @@ void deplacerJoueur(String sens) {
   if (sens == "gauche") {
     tableau[joueurY][joueurX] = 0;
     joueurX++;
-    tableau[joueurY][joueurX] = 1;
+    tableau[joueurY][joueurX] = 4;
     etatButtonPlusPrec = HIGH;
   } else if (sens == "droite") {
     tableau[joueurY][joueurX] = 0;
     joueurX--;
-    tableau[joueurY][joueurX] = 1;
+    tableau[joueurY][joueurX] = 4;
     etatButtonMoinsPrec = HIGH;
   }
 }
